09d085b6c5042f464698847a7321a6af957e98c5
M V Kiran <mada.kiran@s.amity.edu>
2026-03-31 18:31:05 +0530

    Create quick_sort.cpp

---
+++ 02_Sorting_Algorithms/quick_sort.cpp
@@ -0,0 +1,6 @@
+// ============================================================
+// FILE: quick_sort.cpp
+// TIME:  O(n log n) avg | O(n²) worst (sorted array)
+// SPACE: O(log n) recursion stack
+// STABLE: NO
+// ============================================================
