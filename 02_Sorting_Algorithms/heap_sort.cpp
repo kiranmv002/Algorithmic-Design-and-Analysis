3ce48006f5d31c187ef5f3c026c454b21c4443ab
M V Kiran <mada.kiran@s.amity.edu>
2026-04-02 17:55:23 +0530

    Update heap_sort.cpp

--- 02_Sorting_Algorithms/heap_sort.cpp
+++ 02_Sorting_Algorithms/heap_sort.cpp
@@ -23,3 +23,29 @@ void heapify(int arr[], int n, int i) {
     }
 }
 
+void heapSort(int arr[], int n) {
+    // Build max heap
+    for (int i = n / 2 - 1; i >= 0; i--)
+        heapify(arr, n, i);
+
+    // Extract elements one by one
+    for (int i = n - 1; i > 0; i--) {
+        swap(arr[0], arr[i]);
+        heapify(arr, i, 0);
+    }
+}
+
+void printArray(int arr[], int n) {
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+    cout << "\n";
+}
+
+int main() {
+    int arr[] = {12, 11, 13, 5, 6, 7};
+    int n = 6;
+    cout << "Before: "; printArray(arr, n);
+    heapSort(arr, n);
+    cout << "After:  "; printArray(arr, n);
+    return 0;
+}
