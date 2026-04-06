35a214aebe1bc6e3d2c56078a2a07d8cd3a0b282
M V Kiran <mada.kiran@s.amity.edu>
2026-04-06 23:31:55 +0530

    Create ternary_search.cpp

---
+++ 03_Searching_Algorithms/ternary_search.cpp
@@ -0,0 +1,6 @@
+// ============================================================
+// FILE: ternary_search.cpp
+// TIME:  O(log₃n)
+// SPACE: O(1) iterative | O(log₃n) recursive
+// NOTE:  Array MUST be sorted
+// ============================================================
