1c568ec5c8ba05cf5d64baa5af7419c8f7d1b57e
M V Kiran <mada.kiran@s.amity.edu>
2026-04-30 18:26:54 +0530

    Create lcs.cpp

---
+++ 06_Dynamic_Programming/codes/lcs.cpp
@@ -0,0 +1,6 @@
+// ============================================================
+// FILE: lcs.cpp
+// TOPIC: Longest Common Subsequence
+// TIME:  O(m×n)   SPACE: O(m×n)
+// Compile: g++ -std=c++17 -o lcs lcs.cpp
+// ============================================================
