e14d6c836502e86b5b10606165018404c635e1f4
M V Kiran <mada.kiran@s.amity.edu>
2026-04-30 18:29:50 +0530

    Update lcs.cpp

--- 06_Dynamic_Programming/codes/lcs.cpp
+++ 06_Dynamic_Programming/codes/lcs.cpp
@@ -4,3 +4,49 @@
 // TIME:  O(m×n)   SPACE: O(m×n)
 // Compile: g++ -std=c++17 -o lcs lcs.cpp
 // ============================================================
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+// LCS Length
+int lcsLength(const string& s1, const string& s2) {
+    int m = s1.size(), n = s2.size();
+    vector<vector<int>> dp(m+1, vector<int>(n+1, 0));
+
+    for (int i = 1; i <= m; i++) {
+        for (int j = 1; j <= n; j++) {
+            if (s1[i-1] == s2[j-1])
+                dp[i][j] = dp[i-1][j-1] + 1;
+            else
+                dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+        }
+    }
+    return dp[m][n];
+}
+
+// Print actual LCS string
+string printLCS(const string& s1, const string& s2) {
+    int m = s1.size(), n = s2.size();
+    vector<vector<int>> dp(m+1, vector<int>(n+1, 0));
+
+    for (int i = 1; i <= m; i++)
+        for (int j = 1; j <= n; j++) {
+            if (s1[i-1] == s2[j-1])
+                dp[i][j] = dp[i-1][j-1] + 1;
+            else
+                dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+        }
+
+    // Backtrack to find LCS string
+    string lcs = "";
+    int i = m, j = n;
+    while (i > 0 && j > 0) {
+        if (s1[i-1] == s2[j-1]) {
+            lcs = s1[i-1] + lcs;
+            i--; j--;
+        } else if (dp[i-1][j] > dp[i][j-1]) i--;
+        else j--;
+    }
+    return lcs;
+}
