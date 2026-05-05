0879a7ebdff4a7905e09e5b1cbc1d9b9bb27e5ea
M V Kiran <mada.kiran@s.amity.edu>
2026-05-05 21:42:50 +0530

    Create matrix_chain.cpp

---
+++ 06_Dynamic_Programming/codes/matrix_chain.cpp
@@ -0,0 +1,6 @@
+// ============================================================
+// FILE: matrix_chain.cpp
+// TOPIC: Matrix Chain Multiplication
+// TIME:  O(n³)   SPACE: O(n²)
+// Compile: g++ -std=c++17 -o matrix matrix_chain.cpp
+// ============================================================
